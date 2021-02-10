44d60330ecb21b71962d9a1e56a1bd023d206eba
unknown <webberic92@yahoo.com>
2021-02-09 23:02:45 -0500

    assignment4 project.

--- ISEC_640_Database_Security/Webb_Assignment4/styles.js
+++ ISEC_640_Database_Security/Webb_Assignment4/styles.js
@@ -30,13 +30,15 @@ function validatePassword(){
   //Must be atleast 10 characters long.
   //Dont need to test for both because we proved both password and confirmPassword are same value in above logic.
   if (password.length >9){
-    document.getElementById("isAtleastTenCharacters").innerHTML = "Both passwords ARE atleast 10 characters.";
+    document.getElementById("isAtleastTenCharacters").innerHTML = "Password DOES have atleast 10 characters.";
   } else{
-    document.getElementById("isAtleastTenCharacters").innerHTML = "Passwords ARE NOT atleast 10 characters in length.";
+    document.getElementById("isAtleastTenCharacters").innerHTML = "Password DOES NOT have atleast 10 characters.";
 
   }
 
-  //Must have at least 1 uppercase
+  //Must have at least 1 uppercase  
+  //Must have atleast 2 lowercase
+
   var i = 0;
   var char = "";
   var isUppercasePresent = false;
@@ -71,80 +73,53 @@ function validatePassword(){
 }
   
 
-
-  //Must have atleast 2 lowercase
-
   //Must have 1 special character
-
-  //Must have 1 number
-
-  document.getElementById("isPasswordValid").innerHTML = "Is Password Valid? : " + isPasswordValid;
-
-
-  }
-
-
-function myAddress() {
-	
-	// Gets value for wallet address..
-  var x = document.getElementById("myAddress").value;
-  document.getElementById("displayAddress").innerHTML = x;
-  document.getElementById("youChose").innerHTML = "You chose the password...";
-  
-  //makes get request.
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        let jsonObj=JSON.parse(this.responseText);
-
-     // traverse the JSON object
-      //WORKS
-       var balance =jsonObj['balance'];
-       //creates table
- 	  var table="<tr><th>Asset ID</th><th>Amount</th></tr>";
- 	  //loops table
- 	  for (var i=0; i<balance.length; i++) {  
-    	   table += "<tr><td>" +
-   	    JSON.stringify(balance[i].asset_symbol) +
-   	    "</td><td>" +
-   	    JSON.stringify(balance[i].amount) +
-   	    "</td></tr>";
-       }
-       document.getElementById("demoTable").innerHTML = table;
-       
-       }
-     
-    if( this.readyState == 4 && this.status == 400) {
-        document.getElementById("demo1").innerHTML = "That is an incorrect wallet address format....";
-
-    }  
-     
-  };
-  xhttp.open("GET", "https://api.neoscan.io/api/main_net/v1/get_balance/" + x, true);
-  xhttp.send();
-  var y = document.getElementById("horizontalLine");
-  y.style.display = "block";
-   
+  if(/^[a-zA-Z0-9]*$/.test(password) == false){
+    document.getElementById("isSpecialCharacterPresent").innerHTML = "Password DOES contain atleast one special character value.";
+    
+  }else{
+  document.getElementById("isSpecialCharacterPresent").innerHTML = "Password DOES NOT contain atleast one special character value.";
 }
 
 
 
+
+//NOT working try REGEX instead???
+  //Must have 1 number
+var isNumberPresentBoolean = false;
+var i=0;
+var char ="";
+
+while (i>password.length){
+
+char = password.charAt(i);
+
+if(isNaN(char)==false){
+  isNumberPresentBoolean = true
+}
+i++;
+
+}
+
+  if(isNumberPresentBoolean){
+    document.getElementById("isNumberPresent").innerHTML = "Password DOES contain atleast one Number.";
+    
+  }else{
+  document.getElementById("isNumberPresent").innerHTML = "Password DOES NOT contain atleast one Number.";
+}
+
+
+  document.getElementById("isPasswordValid").innerHTML = "Is Password Valid? : " + isPasswordValid;
+
+  }
  
-function clearAddress() {
-	  document.getElementById("displayAddress").innerHTML = "Address Cleared, Please enter another...";
-	  document.getElementById("youChose").innerHTML = "";
-	  document.getElementById("demoTable").innerHTML = "";
-	  document.getElementById("demo1").innerHTML = "";
-	  document.getElementById("demo2").innerHTML = "";
-	  table= "";
-      
-	  var y = document.getElementById("horizontalLine");
-	  y.style.display = "none";
-	  
-	  var t = document.getElementById("demoTable");
-	  t.style.display = "none";
+function clear() {
+	  document.getElementById("doPasswordsMatch").innerHTML = "";
+	  document.getElementById("isAtleastTenCharacters").innerHTML = "";
+	  document.getElementById("isUpperCasePresent").innerHTML = "";
+	  document.getElementById("isLowerCasePresentAtLeastTwice").innerHTML = "";
+    document.getElementById("isSpecialCharacterPresent").innerHTML = "";
+    document.getElementById("isNumberPresent").innerHTML = "";
 
 
-	  var x = document.getElementById("myAddress").value;
-	  x=null;
 	}
